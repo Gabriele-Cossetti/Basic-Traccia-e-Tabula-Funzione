2663f0179d1651d9002da327dc275c1a18acced6
Gabriele Cossetti <78553671+Gabriele-Cossetti@users.noreply.github.com>
2022-11-16 17:08:35 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Info
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(772, 688)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.TextBox1.Location = New System.Drawing.Point(790, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(518, 591)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Button1.Location = New System.Drawing.Point(986, 644)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(187, 34)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Traccia e tabula"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1320, 712)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿' a SCUOLA con VISUAL BASIC 2010 EXPRESS
+' http://vbscuola.it
+' Pier Luigi Farri; Giovanni Piotti; Sandro Sbroggiò
+' https://www.edatlas.it/scarica/SIA_3/Capitolo7/MaterialiOnLine/2ApplicazioniGrafiche.pdf
+' © Istituto Italiano Edizioni Atlas - Pag. 6 
+Public Class Form1
+    ' Estremi del piano cartesiano
+    Const minX As Single = -8
+    Const maxX As Single = 8
+    Const minY As Single = -8
+    Const maxY As Single = 8
+    ' Larghezza (= altezza) dell'area del grafico
+    Dim l As Short
+    ' Unità di misura in pixel
+    Dim u As Single
+    ' Coordinate di un punto
+    Dim X0, Y0 As Integer
+    Dim X1, Y1, X2, Y2 As Double
+    ' Oggetto grafico
+    Dim Piano As Graphics
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Piano = PictureBox1.CreateGraphics()
+        ' Dimensioni del PictureBox
+        l = PictureBox1.Size.Width
+        ' Unità di misura
+        u = l / (maxX - minX)
+        ' Origine
+        X0 = PictureBox1.Size.Width / 2        'X0 = u
+        Y0 = PictureBox1.Size.Height / 2       'Y0 = maxY * u
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Piano.Clear(PictureBox1.BackColor)
+        DisegnaAssi()
+        Dim x As Single
+        Dim j As Integer
+        j = 1
+        For x = minX To maxX Step 0.1
+            X1 = X0 + x * u
+            Y1 = Y0 - F(x, j) * u
+            X2 = X0 + (x + 0.1) * u
+            Y2 = Y0 - F(x + 0.1, j) * u
+            Segmento(j, X1, Y1, X2, Y2)
+            'stampa tabulare
+            TextBox1.Text &= ("X= ") & x & ("   Y= ") & F(x, j) & vbCrLf
+        Next x
+    End Sub
+
+    Public Sub DisegnaAssi()
+        Dim pen As New Pen(Color.Black, 1)
+        'Asse x
+        X1 = X0 + minX * u
+        Y1 = Y0
+        X2 = X0 + maxX * u
+        Y2 = Y0
+        Piano.DrawLine(pen, CInt(X1), CInt(Y1), CInt(X2), CInt(Y2))
+        'Asse y
+        X1 = X0
+        Y1 = Y0 - minY * u
+        X2 = X0
+        Y2 = Y0 - maxY * u
+        Piano.DrawLine(pen, CInt(X1), CInt(Y1), CInt(X2), CInt(Y2))
+        pen.Dispose()
+    End Sub
+
+    Public Sub Segmento(ByVal nf As Integer, ByVal a As Double, ByVal b As Double, ByVal c As Double, ByVal d As Double)
+        Dim col As Color
+        col = Color.Red
+        Dim pen As New Pen(col, 1)
+        Piano.DrawLine(pen, CInt(a), CInt(b), CInt(c), CInt(d))
+        pen.Dispose()
+    End Sub
+
+    Public Function F(ByVal i As Single, ByVal nf As Integer) As Double
+        'F = Math.Exp(i)
+        'F = 12.5 * i - 12.5 * i ^ 2
+        F = Math.Sin(i) / i
+        Return (F)
+    End Function
+End Class
+
